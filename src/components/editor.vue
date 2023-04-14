fae6107617c95be2d43e4771f8fb42d24d532dcf
langyamu <1764714737@qq.com>
2023-04-14 22:40:56 +0800

    :wrench:

---
+++ src/components/editor.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import StarterKit from "@tiptap/starter-kit";
+import { EditorContent, useEditor } from '@tiptap/vue-3';
+
+const editor = useEditor({
+  content:'<p>hello world</p>',
+  extensions:[
+    StarterKit
+  ]
+})
+</script>   
+
+<template>
+  <editor-content :editor="editor"></editor-content>
+</template>
+<style lang="css">
+</style>
+<style scoped>
+
+#editor {
+  width: 50vw;
+  height: 50vh;
+}
+</style>
